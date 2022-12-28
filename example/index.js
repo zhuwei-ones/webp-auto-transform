f3ce789b188d7cc8653d706efc91c14c330e53fc
zhuwei-ones <zhuwei@ones.cn>
2022-12-28 15:52:35 +0800

    chore: example update

--- example/index.js
+++ example/index.js
@@ -3,10 +3,10 @@
 const WebpAutoTransform = require("../dist/index")
 
 WebpAutoTransform({
-    entryPath:"./images",
-    outputPath:"./images-webp",
+    entryPath:"./images_all_test",
+    outputPath:"./images_all_test-webp",
 
-    quality:75, // 75 is default value
+    // quality:90, // 75 is default value
     customList:[  // 针对个别图片做定制化
         {
             path:"./images/optimize.png",
@@ -15,8 +15,9 @@ WebpAutoTransform({
     ] ,
     biggerWebpDelete: true, // 转换后更大的webp 图片处理方式， 删除 还是保留，默认删除
     webpExistReplace: true , // 如果webp文件已经存在，是否替换
+    cache:true,
 
     // ...其他 cwebp 支持的参数，参考https://developers.google.com/speed/webp/docs/cwebp
-    lossless:true,
-    alpha_q:9
+    // lossless:true,
+    // alpha_q:9
 })
